3d3d8bb3465d9c31cccb1021bde72db4c15857df
Geng Sun <gsun@iastate.edu>
2023-02-26 21:18:17 -0600

    Connected user service with order service
    added order client

--- shopping-services/src/main/java/com/shopping/service/UserServiceImpl.java
+++ shopping-services/src/main/java/com/shopping/service/UserServiceImpl.java
@@ -1,17 +1,26 @@
 package com.shopping.service;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.common.collect.ImmutableList;
+import com.shopping.client.OrderClient;
 import com.shopping.db.User;
 import com.shopping.db.UserDao;
 import com.shopping.stubs.user.GetUserRequest;
 import com.shopping.stubs.user.GetUserResponse;
 import com.shopping.stubs.user.Gender;
 import com.shopping.stubs.user.UserServiceGrpc;
+import com.shopping.stubs.order.Order;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
 // UserServiceImpl encapsulates the implementation of the gRPC service which gets User details
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
-
+    Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
     UserDao userDao = new UserDao();
 
     // This method is called whenever a client calls the getUser() RPC
@@ -23,13 +32,26 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
         User user = userDao.getDetails(request.getUsername());
 
         // Build a response object for the user with the required information
-        GetUserResponse userResponse = GetUserResponse.newBuilder()
+        GetUserResponse.Builder userResponseBuilder = GetUserResponse.newBuilder()
                 .setId(user.getId())
                 .setUsername(user.getUsername())
                 .setName(user.getName())
                 .setAge(user.getAge())
-                .setGender(Gender.valueOf(user.getGender()))
+                .setGender(Gender.valueOf(user.getGender()));
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:50052")
+                .usePlaintext()
                 .build();
+
+        OrderClient orderClient = new OrderClient(channel);
+        ImmutableList<Order> orders = orderClient.getOrders(userResponseBuilder.getId());
+        GetUserResponse userResponse = userResponseBuilder.setNoOfOrders(orders.size()).build();
+
+        try {
+            channel.shutdown().awaitTermination(4, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "cannot shutdown channel", e);
+        }
 
         // Send the response and complete the stream
         responseObserver.onNext(userResponse);
